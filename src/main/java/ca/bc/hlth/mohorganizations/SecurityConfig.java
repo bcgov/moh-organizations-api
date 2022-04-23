285a4998e9eb9f3fe2803ed3728ce64b2044d7c0
David Sharpe <david.a.sharpe@cgi.com>
2022-04-23 11:56:05 -0700

    Minor refactor: rearrange SecurityConfig.

--- src/main/java/ca/bc/hlth/mohorganizations/SecurityConfig.java
+++ src/main/java/ca/bc/hlth/mohorganizations/SecurityConfig.java
@@ -13,14 +13,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors()
-            .and()
-              .authorizeRequests()
-                .anyRequest()
-                  .authenticated()
-            .and()
-              .oauth2ResourceServer()
-                .jwt();
+        http.authorizeRequests()
+                .anyRequest().authenticated()
+                .and().cors()
+                .and().oauth2ResourceServer().jwt();
     }
 
     @Bean
