96b6aa00702ca54b0562f8a17b07c579915590d2
David Sharpe <david.a.sharpe@cgi.com>
2022-04-23 11:36:30 -0700

    Add test that uses JWT.

--- src/test/java/ca/bc/hlth/mohorganizations/OrganizationsControllerTest.java
+++ src/test/java/ca/bc/hlth/mohorganizations/OrganizationsControllerTest.java
@@ -1,20 +1,36 @@
 package ca.bc.hlth.mohorganizations;
 
+import net.minidev.json.JSONObject;
+import net.minidev.json.parser.JSONParser;
+import net.minidev.json.parser.ParseException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-import javax.naming.NamingException;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class OrganizationsControllerTest {
+
+    Logger logger = LoggerFactory.getLogger(OrganizationsControllerTest.class);
+
+    private static final JSONParser jsonParser = new JSONParser(JSONParser.DEFAULT_PERMISSIVE_MODE);
+
+    // The credentials used to retrieve an access token.
+    // e.g. client_id=PIDP-SERVICE&client_secret=some_secret&scope=email&grant_type=client_credentials
+    // e.g. client_id=admin-cli&username=admin&password=admin&grant_type=some_password
+    @Value("${ORGANIZATIONS_API_TOKEN_CREDENTIALS}")
+    private String credentials;
 
     @LocalServerPort
     private int port;
@@ -22,7 +38,7 @@ class OrganizationsControllerTest {
     private String urlUnderTest;
 
     @Autowired
-    private TestRestTemplate restTemplate;
+    private WebTestClient webClient;
 
     @BeforeEach
     public void init() {
@@ -32,8 +48,36 @@ class OrganizationsControllerTest {
 
     @Test
     public void testGetOrganizations_noToken_unauthorized() {
-        ResponseEntity<Object> response = restTemplate.getForEntity(urlUnderTest, Object.class);
-        assertEquals(HttpStatus.UNAUTHORIZED, response.getStatusCode());
+        webClient.get().uri(urlUnderTest).exchange().expectStatus().isUnauthorized();
     }
+
+    @Test
+    public void testGetOrganizations_withToken_getOrganizations() throws Exception {
+
+        String accessToken = getKcAccessToken();
+
+        webClient.get()
+                .uri(urlUnderTest)
+                .header("Authorization", "Bearer " + accessToken)
+                .exchange()
+                .expectStatus().isOk();
+    }
+
+    private String getKcAccessToken() throws IOException, InterruptedException, ParseException {
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://common-logon-dev.hlth.gov.bc.ca/auth/realms/moh_applications/protocol/openid-connect/token"))
+                .header("Accept", "application/json")
+                .header("Content-Type", "application/x-www-form-urlencoded")
+                .POST(HttpRequest.BodyPublishers.ofString(credentials))
+                .build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        JSONObject responseBodyAsJson = (JSONObject) jsonParser.parse(response.body());
+
+        return responseBodyAsJson.get("access_token").toString();
+    }
+
 
 }
