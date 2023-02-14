ea3842da9059db36deb058ee401ca738556db249
Florek <filip.florek@cgi.com>
2023-02-14 15:15:04 -0700

    remove sample database tables and roles

--- terraform-dev/infrastructure/db.tf
+++ terraform-dev/infrastructure/db.tf
@@ -1,27 +1,3 @@
-
-resource "aws_dynamodb_table" "startup_sample_table" {
-  name           = var.db_name
-  hash_key       = "pid"
-  range_key      = "createdAt"
-  read_capacity  = 1
-  write_capacity = 1
-
-  attribute {
-    name = "pid"
-    type = "S"
-  }
-
-  attribute {
-    name = "createdAt"
-    type = "S"
-  }
-
-  tags = local.common_tags
-}
-
-
-## TODO: Document this.
-
 resource "aws_dynamodb_table" "organization_table" {
   name           = "Organization"
   hash_key       = "id"
@@ -34,5 +10,3 @@ resource "aws_dynamodb_table" "organization_table" {
   }
 
 }
-
-## END TODO
